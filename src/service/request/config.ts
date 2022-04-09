acf9fa0d1de2fc52961c1bfb7a3b56527384a92f
coderhxl <743137771@qq.com>
2022-04-09 13:38:21 +0800

    build(base_url): 更新生产环境BASE_URL

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -19,9 +19,9 @@ const TIME_OUT = 10000
 if (process.env.NODE_ENV === 'development') {
   BASE_URL = '/api'
 } else if (process.env.NODE_ENV === 'production') {
-  BASE_URL = 'https://code.org/prod'
+  BASE_URL = '/api'
 } else {
-  BASE_URL = 'https://code.org/test'
+  BASE_URL = 'https://codehxl.org/test'
 }
 
 export { BASE_URL, TIME_OUT }
