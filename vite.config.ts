a5a9941b903b0d00e757dda9f2024e7d9ae15bfc
coderhxl <coderhxl@outlook.com>
2022-04-09 17:24:11 +0800

    test

--- vite.config.ts
+++ vite.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     port: 3000,
     host: '0.0.0.0',
     proxy: {
-      '^/api/*': {
+      '^/api/.*': {
         target: 'http://152.136.185.210:5000',
         changeOrigin: true,
         rewrite: (p) => p.replace(/^\/api/, '')
