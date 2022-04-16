80b1028ffc82523820b04d5a0692243507c02f46
coderhxl <coderhxl@outlook.com>
2022-04-16 17:01:32 +0800

    style(page): editor

--- src/views/main/story/chat/chat.vue
+++ src/views/main/story/chat/chat.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="chat">
-    <xl-editor editorId="chat"></xl-editor>
+    <xl-editor editorId="chat" :mode="editorMode"></xl-editor>
   </div>
 </template>
 
 <script setup lang="ts">
 import XlEditor from '@/base-ui/editor'
+import { reactiveWidth } from '@/utils/reactive-width'
+
+const editorMode = reactiveWidth('simple', 'default') as unknown as
+  | 'default'
+  | 'simple'
 </script>
 
 <style scoped></style>
