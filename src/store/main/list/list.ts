a862b66bee2dad2d170f67e79e95dd1775dbeaec
coderhxl <coderhxl@outlook.com>
2022-08-22 18:30:37 +0800

    类型修改

--- src/store/main/list/list.ts
+++ src/store/main/list/list.ts
@@ -7,101 +7,103 @@ import {
   newPageData
 } from '@/service/main/list/list'
 
-import { IListState } from './types'
+import { IListState, IGetters, IActions, IListStateKey } from './types'
 
-export const useListStore = defineStore({
-  id: 'list',
-  state: (): IListState => ({
-    // 系统管理
-    usersList: [],
-    usersCount: 0,
-    departmentList: [],
-    departmentCount: 0,
-    menuList: [],
-    menuCount: 0,
-    roleList: [],
-    roleCount: 0,
-    // 商品中心
-    categoryList: [],
-    categoryCount: 0,
-    goodsList: [],
-    goodsCount: 0,
-    // 动态
-    momentList: [],
-    momentCount: 0
-  }),
-  getters: {
-    pageListData(state: any) {
-      return (pageName: string) => {
-        return state[`${pageName}List`]
+export const useListStore = defineStore<string, IListState, IGetters, IActions>(
+  {
+    id: 'list',
+    state: () => ({
+      // 系统管理
+      usersList: [],
+      usersCount: 0,
+      departmentList: [],
+      departmentCount: 0,
+      menuList: [],
+      menuCount: 0,
+      roleList: [],
+      roleCount: 0,
+      // 商品中心
+      categoryList: [],
+      categoryCount: 0,
+      goodsList: [],
+      goodsCount: 0,
+      // 动态
+      momentList: [],
+      momentCount: 0
+    }),
+    getters: {
+      pageListData(state: any) {
+        return (pageName: string) => {
+          return state[`${pageName}List`]
+        }
+      },
+      pageListCount(state: any) {
+        return (pageName: string) => {
+          return state[`${pageName}Count`]
+        }
       }
     },
-    pageListCount(state: any) {
-      return (pageName: string) => {
-        return state[`${pageName}Count`]
+    actions: {
+      async getPageListAction(payload: any) {
+        // 1.获取pageUrl
+        const pageName: string = payload.pageName
+        const pageUrl = `/${pageName}/list`
+
+        // 2.在页面发送请求
+        const pageResult = await getPageListData(pageUrl, payload.queryInfo)
+        const { list, totalCount } = pageResult.data
+
+        // 3.将数据存储到state中
+        const changeList = `${pageName}List` as IListStateKey
+        const changeCount = `${pageName}Count` as IListStateKey
+        this[changeList] = list
+        this[changeCount] = totalCount
+      },
+      async deletePageDataAction(payload: any) {
+        // 1.获取url
+        const { pageName, id } = payload
+        const url = `${pageName}/${id}`
+
+        // 2.发送删除网络请求
+        await deletePageData(url)
+
+        // 3.重新发送请求
+        this.getPageListAction({
+          pageName,
+          queryInfo: {
+            offset: 0,
+            size: 10
+          }
+        })
+      },
+      async editPageDataAction(payload: any) {
+        const { pageName, editData, id } = payload
+        const url = `/${pageName}/${id}`
+
+        await editPageData(url, editData)
+
+        this.getPageListAction({
+          pageName,
+          queryInfo: {
+            offset: 0,
+            size: 10
+          }
+        })
+      },
+      async createPageDataAction(payload: any) {
+        const { pageName, newData } = payload
+        const url = `/${pageName}`
+
+        await newPageData(url, newData)
+
+        this.getPageListAction({
+          pageName,
+          queryInfo: {
+            offset: 0,
+            size: 10
+          }
+        })
       }
-    }
-  },
-  actions: {
-    async getPageListAction(payload: any) {
-      // 1.获取pageUrl
-      const pageName: string = payload.pageName
-      const pageUrl = `/${pageName}/list`
-
-      // 2.在页面发送请求
-      const pageResult = await getPageListData(pageUrl, payload.queryInfo)
-      const { list, totalCount } = pageResult.data
-
-      // 3.将数据存储到state中
-      const changeList = `${pageName}List`
-      const changeCount = `${pageName}Count`
-      this[changeList] = list
-      this[changeCount] = totalCount
-    },
-    async deletePageDataAction(payload: any) {
-      // 1.获取url
-      const { pageName, id } = payload
-      const url = `${pageName}/${id}`
-
-      // 2.发送删除网络请求
-      await deletePageData(url)
-
-      // 3.重新发送请求
-      this.getPageListAction({
-        pageName,
-        queryInfo: {
-          offset: 0,
-          size: 10
-        }
-      })
-    },
-    async editPageDataAction(payload: any) {
-      const { pageName, editData, id } = payload
-      const url = `/${pageName}/${id}`
-
-      await editPageData(url, editData)
-
-      this.getPageListAction({
-        pageName,
-        queryInfo: {
-          offset: 0,
-          size: 10
-        }
-      })
-    },
-    async createPageDataAction(payload: any) {
-      const { pageName, newData } = payload
-      const url = `/${pageName}`
-
-      await newPageData(url, newData)
-
-      this.getPageListAction({
-        pageName,
-        queryInfo: {
-          offset: 0,
-          size: 10
-        }
-      })
     }
   }
-})
+)
--- src/store/main/list/types.ts
+++ src/store/main/list/types.ts
@@ -1,3 +1,14 @@
+interface IMoment {
+  id: number
+  title: string
+  contentHtml: string
+  contentText: string
+  userName: string
+  avatarUrl: string
+  createAt: string
+  updateAt: string
+}
+
 export interface IListState {
   usersList: any[]
   usersCount: number
@@ -13,6 +24,22 @@ export interface IListState {
   goodsList: any[]
   goodsCount: number
 
-  momentList: any[]
+  momentList: IMoment[]
   momentCount: number
 }
+
+export interface IGetters {
+  pageListData(state: any): (pageName: string) => any
+  pageListCount(state: any): (pageName: string) => any
+
+  [key: string]: any
+}
+
+export interface IActions {
+  getPageListAction(payload: any): Promise<void>
+  deletePageDataAction(payload: any): Promise<any>
+  editPageDataAction(payload: any): Promise<any>
+  createPageDataAction(payload: any): Promise<any>
+}
+
+export type IListStateKey = keyof IListState
